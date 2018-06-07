8d6f25a0403de85ad38f1b2e0c0eeed4a2f1266a
Matteo Gabriele <m.gabriele.dev@gmail.com>
2018-06-07 10:50:09 +0200

    variable name check

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <p>{{ $foo }}</p>
+    <p>{{ $$foo }}</p>
+    <p>{{ $_foo }}</p>
+    <p>{{ _foo }}</p>
+    <p>{{ foo }}</p>
+  </div>
+</template>
+
+<script>
+import mixin from './mixin'
+
+export default {
+  name: 'app',
+  mixins: [mixin]
+}
+</script>
---
+++ src/mixin.js
@@ -0,0 +1,11 @@
+export default {
+  data () {
+    return {
+      foo: 'foo',
+      $$foo: '$$foo',
+      $foo: '$foo',
+      $_foo: '$_foo',
+      _foo: '_foo'
+    }
+  }
+}
